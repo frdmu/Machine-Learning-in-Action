7794221349e053f022efc577ca29607f3275c223
frdmu <2320899425@qq.com>
2020-10-28 04:36:16 -0400

    GMM1.py still undone and implement E-step and M-step

--- pattern_recognition/assignment2/GMM1.py
+++ pattern_recognition/assignment2/GMM1.py
@@ -20,23 +20,48 @@ class GMM(object):
         self.mu = np.random.uniform(tempMin-1, tempMax+1, self.k*self.dim).reshape(self.k, self.dim)
 
         self.sigma = np.zeros((self.k, self.dim, self.dim))
-         # self.sigma's inverse matrix 
-        self.sigmaI = np.zeros((self.k, self.dim, self.dim))
         i = 0
         while i < self.k:
             self.sigma[i] = np.diag(np.random.rand(dim) + 2)
             i += 1
-        i = 0
-        while i < self.k:
-            self.sigmaI[i] = np.array(np.mat(self.sigma[i]).I)
-             i += 1
-       
-       # Normalized 
-       self.pLabel = np.random.rand(self.data.shape[0], k)
-       tempSum = np.sum(self.pLabel, axis=1)
-       self.pLabel = np.divide(self.pLabel.transpose(), tempSum).transpose()
+      
+       # probablility P(yi = k | xi, alpha, mu, sigma)
+       self.pLabel = np.zeros((self.data.shape[0], k))
+    
+    def train(self):
+        e = 1
+        # the number of iterate 
+        cnt = 0
+        while e > 1e-30:
+            cnt += 1
+            # E step 
+            i = 0
+            while i < self.data.shape[0]:
+                 # calcute P(yi = k | xi, alpha, mu, sigma) 
+                self.pLabel[i] = self.calNormal(self.data[i])
+                i += 1
+            # M step
+             # update alpha
+            tempAlpha = copy.deepcopy(self.alpha)
+            self.alpha = np.mean(self.pLabel, axis=0)
 
+             # update mu
+            tempMu = copy.deepcopy(self.mu)
+            i = 0
+            while i < self.k:
+                self.mu[i] = np.array((np.mat(self.pLabel[:, i]) * np.mat(self.data)) / np.sum(self.pLabel[:, i]))
+                i += 1
 
+             # update sigma
+            tempSigma = copy.deepcopy(self.sigma)
+            i = 0
+            while i < self.k:
+                tempDif = self.data - self.mu[i]
+                self.sigma[i] =  np.dot(tempDif.T, tempDif*self.pLbael[:, i].reshape(self.pLabel.shape[0], 1)) / np.sum(self.pLabel[:, i])
+                i += 1
+
+            e = np.sum((self.alpha-tmepAlpha)**2) + np.sum((self.mu-tempMu)**2) + np.sum((self.sigma-tempSigma)**2)
+            print("[{}]:".format(cnt) + "error is {}.".format(e))
 
 def readData(data, file):
     csv_object = csv.reader(open(file, 'r'))
