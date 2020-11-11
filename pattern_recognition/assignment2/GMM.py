015175e0cc8e5d37c03f62fbea0faa26205a956e
frdmu <2320899425@qq.com>
2020-11-11 18:57:03 +0800

    GMM done but without test

--- pattern_recognition/assignment2/GMM.py
+++ pattern_recognition/assignment2/GMM.py
@@ -6,14 +6,14 @@ from scipy.stats import multivariate_normal
 
 class GMM(object):
     def __init__(self, data, k, dim):
-        self.data = np.array(data).astype(float)
-         # the number of clustering 
+        self.data = np.array(data).astype(float)/100
+         # Gaussian number
         self.k = k
          # the number of data feature,ie. data dimension
         self.dim = dim
        
         # initialize parameters: alpha, mu, sigma
-        self.alpha = np.random.rand(k)
+        self.alpha = np.random.rand(self.k)
         self.alpha /= np.sum(self.alpha)
 
         tempMax = np.amax(self.data)
@@ -23,7 +23,7 @@ class GMM(object):
         self.sigma = np.zeros((self.k, self.dim, self.dim))
         i = 0
         while i < self.k:
-            self.sigma[i] = np.diag(np.random.rand(self.dim) + 2)
+            self.sigma[i] = np.diag(np.random.rand(self.dim) + 1000)
             i += 1
       
         # probablility P(yi = k | xi, alpha, mu, sigma)
@@ -48,7 +48,7 @@ class GMM(object):
         e = 1
         # the number of iterate 
         cnt = 0
-        while e > 1e-30:
+        while e > 1e-10:
             cnt += 1
             # E step 
             i = 0
@@ -89,7 +89,7 @@ class GMM(object):
         for row in self.sigma:
             row = row.reshape(1, row.size)[0]
             writer3.writerow(row)
-   
+  
     def loadPara(self, alphaFile, muFile, sigmaFile):
         tempAlpha = []
         tempMu = []
@@ -134,7 +134,26 @@ def getAccuracy(l, flag):
     else: 
         return total1 / (total0 + total1)
 
+def MnistDataDivide(MnistData, MnistLabel):
+    MnistData = np.array(MnistData).astype(float) 
+    MnistLabel = np.array(MnistLabel).T[0].astype(float)
+    data =  []
+    i = 0 
+    while i < 10:
+        idx = MnistLabel == i
+        data.append(MnistData[idx])
+        i += 1
+    return data
+
+def compare(predictMnistLabel, MnistLabel):
+    print("type: {}".format(type(MnistLabel))) 
+    MnistLabel = np.array(MnistLabel).T[0].astype(float)
+    print("MnistLabel.shape: {}".format(MnistLabel.shape)) 
+    temp = np.ones(MnistLabel.shape[0]) 
+    return np.sum(temp[predictMnistLabel == MnistLabel]) / MnistLabel.shape[0]
+
 if __name__ == '__main__': 
+	"""
 #================================
 # The simulated data test section
 #================================
@@ -157,14 +176,73 @@ if __name__ == '__main__':
     model1.savePara('para/alpha1.csv', 'para/mu1.csv', 'para/sigma1.csv')
     model2.savePara('para/alpha2.csv', 'para/mu2.csv', 'para/sigma2.csv')
   
-    #model1.loadPara('para/alpha1.csv', 'para/mu1.csv', 'para/sigma1.csv')
-    #model2.loadPara('para/alpha2.csv', 'para/mu2.csv', 'para/sigma2.csv')
-
     modelSet = [model1, model2]
     w = np.array([0.5, 0.5])
     
     print(getAccuracy(predict(testData1, modelSet, w), 0))
     print(getAccuracy(predict(testData2, modelSet, w), 1))
+	"""
 
+#=============================
+# MNIST data set test section
+#=============================
+    trainMnistData = []
+    trainMnistLabel = []
+    testMnistData = []
+    testMnistLabel = []
 
+    readData(trainMnistData, 'TrainSamples.csv')
+    readData(trainMnistLabel, 'TrainLabels.csv')
+    readData(testMnistData, 'TestSamples.csv')
+    readData(testMnistLabel, 'TestLabels.csv')
 
+    trainMnistDataSet = MnistDataDivide(trainMnistData, trainMnistLabel)
+
+    model0 = GMM(trainMnistDataSet[0], 5, 17)
+    model1 = GMM(trainMnistDataSet[1], 5, 17)
+    model2 = GMM(trainMnistDataSet[2], 5, 17)
+    model3 = GMM(trainMnistDataSet[3], 5, 17)
+    model4 = GMM(trainMnistDataSet[4], 5, 17)
+    model5 = GMM(trainMnistDataSet[5], 5, 17)
+    model6 = GMM(trainMnistDataSet[6], 5, 17)
+    model7 = GMM(trainMnistDataSet[7], 5, 17)
+    model8 = GMM(trainMnistDataSet[8], 5, 17)
+    model9 = GMM(trainMnistDataSet[9], 5, 17)
+
+    model0.train()
+    model1.train()
+    model2.train()
+    model3.train()
+    model4.train()
+    model5.train()
+    model6.train()
+    model7.train()
+    model8.train()
+    model9.train()
+
+    model0.savePara('MnistPara/alpha5_0.csv', 'MnistPara/mu5_0.csv', 'MnistPara/sigma5_0.csv')
+    model1.savePara('MnistPara/alpha5_1.csv', 'MnistPara/mu5_1.csv', 'MnistPara/sigma5_1.csv')
+    model2.savePara('MnistPara/alpha5_2.csv', 'MnistPara/mu5_2.csv', 'MnistPara/sigma5_2.csv')
+    model3.savePara('MnistPara/alpha5_3.csv', 'MnistPara/mu5_3.csv', 'MnistPara/sigma5_3.csv')
+    model4.savePara('MnistPara/alpha5_4.csv', 'MnistPara/mu5_4.csv', 'MnistPara/sigma5_4.csv')
+    model5.savePara('MnistPara/alpha5_5.csv', 'MnistPara/mu5_5.csv', 'MnistPara/sigma5_5.csv')
+    model6.savePara('MnistPara/alpha5_6.csv', 'MnistPara/mu5_6.csv', 'MnistPara/sigma5_6.csv')
+    model7.savePara('MnistPara/alpha5_7.csv', 'MnistPara/mu5_7.csv', 'MnistPara/sigma5_7.csv')
+    model8.savePara('MnistPara/alpha5_8.csv', 'MnistPara/mu5_8.csv', 'MnistPara/sigma5_8.csv')
+    model9.savePara('MnistPara/alpha5_9.csv', 'MnistPara/mu5_9.csv', 'MnistPara/sigma5_9.csv')
+
+    #model0.loadPara('MnistPara/alpha5_0.csv', 'MnistPara/mu5_0.csv', 'MnistPara/sigma5_0.csv')
+    #model1.loadPara('MnistPara/alpha5_1.csv', 'MnistPara/mu5_1.csv', 'MnistPara/sigma5_1.csv')
+    #model2.loadPara('MnistPara/alpha5_2.csv', 'MnistPara/mu5_2.csv', 'MnistPara/sigma5_2.csv')
+    #model3.loadPara('MnistPara/alpha5_3.csv', 'MnistPara/mu5_3.csv', 'MnistPara/sigma5_3.csv')
+    #model4.loadPara('MnistPara/alpha5_4.csv', 'MnistPara/mu5_4.csv', 'MnistPara/sigma5_4.csv')
+    #model5.loadPara('MnistPara/alpha5_5.csv', 'MnistPara/mu5_5.csv', 'MnistPara/sigma5_5.csv')
+    #model6.loadPara('MnistPara/alpha5_6.csv', 'MnistPara/mu5_6.csv', 'MnistPara/sigma5_6.csv')
+    #model7.loadPara('MnistPara/alpha5_7.csv', 'MnistPara/mu5_7.csv', 'MnistPara/sigma5_7.csv')
+    #model8.loadPara('MnistPara/alpha5_8.csv', 'MnistPara/mu5_8.csv', 'MnistPara/sigma5_8.csv')
+    #model9.loadPara('MnistPara/alpha5_9.csv', 'MnistPara/mu5_9.csv', 'MnistPara/sigma5_9.csv')
+
+    modelSet = [model0, model1, model2, model3, model4, model5, model6, model7, model8, model9]
+    w = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
+    predictLabel = predict(testMnistData, modelSet, w)
+    print(compare(predictLabel, testMnistLabel))
